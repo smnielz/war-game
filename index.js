9e1c6ac09fa82d38b7698d6d9e4f8660e29f6f4d
smnielz <110817166+smnielz@users.noreply.github.com>
2022-09-13 12:19:13 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,75 @@
+let deckId = ""
+let computerScore = 0
+let myScore = 0
+const mainDiv = document.getElementById("main-div")
+const newDeckBtn = document.getElementById("new-deck")
+const drawBtn = document.getElementById("draw")
+const remainText = document.getElementById("remain")
+const cardsDiv = document.getElementsByClassName("cards")
+const winerH1 = document.getElementById("winer-h1")
+const computerText = document.getElementById("computer-score")
+const userText = document.getElementById("user-score")
+
+function load(){
+    winerH1.textContent = "Game of War"
+    computerText.textContent = "Computer score: 0"
+    userText.textContent = "Your score: 0"
+    cardsDiv[0].children[1].innerHTML = ``
+    cardsDiv[0].children[2].innerHTML = ``
+}
+
+function getNewDeck(){
+    load()
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+        .then(res => res.json())
+        .then(data => {
+            deckId = data.deck_id
+            drawBtn.disabled = false
+            remainText.textContent = `Remaining cards: ${data.remaining}`
+        })
+}
+
+
+function getResults(data){
+    const card1 = data[0].value
+    const card2 = data[1].value
+    let cardValues = ["2", "3", "4", "5", "6", "7", "8", "9",
+        "10", "JACK", "QUEEN", "KING", "ACE"]
+    if(cardValues.indexOf(card1) > cardValues.indexOf(card2)){
+        winerH1.textContent = "Computer wins!"
+        computerScore++
+        computerText.textContent = `Computer score: ${computerScore}`
+    }
+    else if(cardValues.indexOf(card1) < cardValues.indexOf(card2)){
+        winerH1.textContent = "You win!"
+        myScore++
+        userText.textContent = `Your score: ${myScore}`
+    }
+    else{
+        winerH1.textContent = "War!"
+    }
+}
+
+newDeckBtn.addEventListener("click", getNewDeck)
+drawBtn.addEventListener("click", function(){
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data =>{
+            remainText.textContent = `Remaining cards: ${data.remaining}`
+            cardsDiv[0].children[1].innerHTML = `<img src="${data.cards[0].image}">`
+            cardsDiv[0].children[2].innerHTML = `<img src="${data.cards[1].image}">`
+            getResults(data.cards)
+            if(data.remaining === 0){
+                if(computerScore > myScore){
+                    winerH1.textContent = "The computer won the game!"
+                }
+                else if(computerScore < myScore){
+                    winerH1.textContent = "You won the game!"
+                }
+                else{
+                    winerH1.textContent = "It's a tie game!"
+                }
+                drawBtn.disabled = true
+            }
+        })
+})
